5265f7cfa1a8a61a768cf1dfcaef4e7a9443d366
Luiz Jacson <luiz.jacson@souunit.com.br>
2022-08-02 18:50:02 -0300

    Criação de votação

--- poll/views.py
+++ poll/views.py
@@ -11,11 +11,13 @@ def Create_poll(request):
     if request.method == 'GET':
         return render(request, 'create_poll.html')
     elif request.method == 'POST' and request.POST.get('create_poll'):
-        quantidade = int(request.POST.get('qtde'))
+        quantidade = request.POST.get('qtde')
         pergunta = request.POST.get('texto_pergunta')
         pergunta_criada = Pergunta.objects.create(texto_pergunta = pergunta)
-        alternativa = request.POST.get('texto_alternativa')
-        Resposta.objects.create(fk_id_pergunta = pergunta_criada, texto_resposta = alternativa)
+        print(int(quantidade))
+        for c in range(0,int(quantidade)):
+            alternativa = request.POST.get(f'texto_alternativa{c+1}')
+            Resposta.objects.create(fk_id_pergunta = pergunta_criada, texto_resposta = alternativa)
         return render(request, 'create_poll.html')
     elif request.method == 'POST' and request.POST.get('add_resp'):
         quantidade = request.POST.get('qtde')
