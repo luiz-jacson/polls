e8877747e87c366ded2b19546c79b2edfa95d4f5
Luiz Jacson <luiz.jacson@souunit.com.br>
2022-08-10 00:41:11 -0300

    divisão da home me diversas páginas

--- poll/urls.py
+++ poll/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from poll.views import Create_poll, Home, Respostas, Resultados
+from poll.views import Create_poll, Home, Respostas, Resultados,Home_pagina
 urlpatterns = [
     path('', Home, name = 'home'),
+    path('home/<int:pagina>/', Home_pagina, name = 'home_pagina'),
     path('create/', Create_poll, name='create'),
     path('vote/<int:pk>/', Respostas, name = 'vote'),
     path('results/<int:pk>/', Resultados, name = 'result'),
--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,4 @@
+import math
 from msilib.schema import Class
 from tkinter.messagebox import QUESTION
 from django.shortcuts import redirect, render
@@ -38,14 +39,19 @@ def Create_poll(request):
     
         
 
-
+def n_paginas(lista_perguntas):
+    num = lista_perguntas.count()
+    return math.ceil(num/10)
 
 
 def Home(request):
     if request.method == 'GET':
         lista_perguntas = Pergunta.objects.all()
+        npaginas = n_paginas(lista_perguntas)
+        lista_perguntas = Pergunta.objects.all()[:10]
         context = {
-            'perguntas':lista_perguntas
+            'perguntas':lista_perguntas,
+            'paginas':[x+1 for x in range(0,npaginas)]
         }
         return render(request, 'home.html', context)
     elif request.method == 'POST' and  request.POST.get('procurar'):
@@ -54,6 +60,25 @@ def Home(request):
             'perguntas':lista_perguntas
         }
         return render(request, 'home.html', context)
+
+
+def Home_pagina(request,pagina):
+    if request.method == 'GET':
+        lista_perguntas = Pergunta.objects.all()
+        npaginas = n_paginas(lista_perguntas)
+        lista_perguntas = Pergunta.objects.all()[((pagina-1)*10):(pagina*10)]
+        context = {
+            'perguntas':lista_perguntas,
+            'paginas':[x+1 for x in range(0,npaginas)]
+        }
+        return render(request, 'home_pagina.html', context)
+    elif request.method == 'POST' and  request.POST.get('procurar'):
+        lista_perguntas = Pergunta.objects.all().filter(texto_pergunta__icontains = request.POST.get('pergunta_procurada'))
+        context = {
+            'perguntas':lista_perguntas
+        }
+        return render(request, 'home_pagina.html', context)
+
 
         
 
