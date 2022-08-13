e154a8237dd9cab89076d0c395196ea0d5b90cae
Luiz Jacson <luiz.jacson@souunit.com.br>
2022-08-13 05:16:28 -0300

    img fix

--- polls/settings.py
+++ polls/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+
+]
+
 STATIC_URL = 'static/'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
